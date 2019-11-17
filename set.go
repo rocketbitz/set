ceab4af42f1d48ff8a44b8bed839368131975211
Christopher Ryan <clryan22@gmail.com>
2019-11-17 13:52:16 -0500

    unsafe set

--- set.go
+++ set.go
@@ -25,21 +25,38 @@ type Set interface {
 
 type set struct {
 	sync.RWMutex
-	m   map[interface{}]int32
-	len int32
+	m    map[interface{}]int32
+	len  int32
+	safe bool
 }
 
-// New declares a new set.
+// New declares a new thread-safe set.
 func New() Set {
 	return &set{
-		m: make(map[interface{}]int32),
+		m:    make(map[interface{}]int32),
+		safe: true,
 	}
 }
 
-// NewFromSlice declares a new set from a
-// supplied slice.
+// NewUnsafe declares a new thread-unsafe set.
+func NewUnsafe() Set {
+	return &set{m: make(map[interface{}]int32)}
+}
+
+// NewFromSlice declares a new thread-safe set
+// from a supplied slice.
 func NewFromSlice(slc []interface{}) Set {
 	s := New()
+	for _, v := range slc {
+		s.Add(v)
+	}
+	return s
+}
+
+// NewUnsafeFromSlice declares a new thread-
+// unsafe set from a supplied slice.
+func NewUnsafeFromSlice(slc []interface{}) Set {
+	s := NewUnsafe()
 	for _, v := range slc {
 		s.Add(v)
 	}
@@ -52,6 +69,8 @@ func (s *set) Len() int {
 	return int(s.len)
 }
 
+// At retrives the value in the set at the
+// supplied index.
 func (s *set) At(i int) interface{} {
 	for k, v := range s.m {
 		if int(v) == i {
@@ -63,38 +82,51 @@ func (s *set) At(i int) interface{} {
 
 // Add a value to the set.
 func (s *set) Add(value interface{}) {
-	s.Lock()
+	if s.safe {
+		s.Lock()
+		defer s.Unlock()
+	}
+
 	if _, loaded := s.m[value]; !loaded {
 		s.m[value] = s.len
 		atomic.AddInt32(&s.len, 1)
 	}
-	s.Unlock()
 }
 
 // Remove a value from the set.
 func (s *set) Remove(value interface{}) (removed bool) {
-	s.Lock()
+	if s.safe {
+		s.Lock()
+		defer s.Unlock()
+	}
+
 	_, removed = s.m[value]
 	delete(s.m, value)
 	atomic.CompareAndSwapInt32(&s.len, s.len, s.len-1)
-	s.Unlock()
+
 	return
 }
 
 // Contains returns true if the supplied value
 // exists in the set, otherwise false.
 func (s *set) Contains(value interface{}) (contained bool) {
-	s.RLock()
+	if s.safe {
+		s.RLock()
+		defer s.RUnlock()
+	}
+
 	_, contained = s.m[value]
-	s.RUnlock()
+
 	return
 }
 
 // Index of the specified value in the set. -1
 // if the set does not contain the value
 func (s *set) Index(value interface{}) int {
-	s.RLock()
-	defer s.RUnlock()
+	if s.safe {
+		s.RLock()
+		defer s.RUnlock()
+	}
 
 	i, ok := s.m[value]
 	if ok {
@@ -106,26 +138,33 @@ func (s *set) Index(value interface{}) int {
 
 // Replace a value in the set
 func (s *set) Replace(old, new interface{}) (replaced bool) {
-	s.Lock()
+	if s.safe {
+		s.Lock()
+		defer s.Unlock()
+	}
+
 	if v, loaded := s.m[old]; loaded {
 		delete(s.m, old)
 		s.m[new] = v
 		replaced = true
 	}
-	s.Unlock()
+
 	return
 }
 
 // Slice returns the values held in the set
 // in the form of a Go slice object.
 func (s *set) Slice() (slc []interface{}) {
-	s.RLock()
+	if s.safe {
+		s.RLock()
+		defer s.RUnlock()
+	}
+
 	slc = make([]interface{}, s.len)
 
 	for k, v := range s.m {
 		slc[v] = k
 	}
 
-	s.RUnlock()
 	return
 }
--- set_test.go
+++ set_test.go
@@ -11,7 +11,7 @@ type SetTestSuite struct {
 	suite.Suite
 }
 
-func (s *SetTestSuite) TestSet() {
+func (s *SetTestSuite) TestSafe() {
 	testSet := New()
 
 	assert.Equal(s.T(), 0, testSet.Len())
@@ -45,6 +45,47 @@ func (s *SetTestSuite) TestSet() {
 	assert.False(s.T(), testSet.Contains("test1"))
 
 	testSet = NewFromSlice([]interface{}{"one", "two"})
+	assert.Equal(s.T(), 2, testSet.Len())
+	assert.True(s.T(), testSet.Replace("two", "three"))
+	assert.Equal(s.T(), "three", testSet.Slice()[1])
+	assert.Equal(s.T(), 1, testSet.Index("three"))
+	assert.Equal(s.T(), -1, testSet.Index("zero"))
+}
+
+func (s *SetTestSuite) TestUnsafe() {
+	testSet := NewUnsafe()
+
+	assert.Equal(s.T(), 0, testSet.Len())
+
+	testSet.Add("test0")
+	assert.Equal(s.T(), 1, testSet.Len())
+
+	testSet.Add("test1")
+	assert.Equal(s.T(), 2, testSet.Len())
+
+	testSet.Add("test1")
+	assert.Equal(s.T(), 2, testSet.Len())
+	assert.Equal(s.T(), "test1", testSet.At(1).(string))
+	assert.Nil(s.T(), testSet.At(2))
+
+	slc := testSet.Slice()
+
+	assert.Len(s.T(), slc, 2)
+
+	assert.Equal(s.T(), "test0", slc[0])
+	assert.Equal(s.T(), "test1", slc[1])
+
+	assert.True(s.T(), testSet.Contains("test0"))
+	assert.True(s.T(), testSet.Contains("test1"))
+	assert.False(s.T(), testSet.Contains("test2"))
+
+	testSet.Remove("test1")
+
+	assert.Equal(s.T(), 1, testSet.Len())
+	assert.True(s.T(), testSet.Contains("test0"))
+	assert.False(s.T(), testSet.Contains("test1"))
+
+	testSet = NewUnsafeFromSlice([]interface{}{"one", "two"})
 	assert.Equal(s.T(), 2, testSet.Len())
 	assert.True(s.T(), testSet.Replace("two", "three"))
 	assert.Equal(s.T(), "three", testSet.Slice()[1])
